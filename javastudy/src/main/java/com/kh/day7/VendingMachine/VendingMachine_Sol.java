a94d21e3542d949d5ef90b955c69aff9c142a3f2
kjheve <120702629+kjheve@users.noreply.github.com>
2023-12-29 09:09:09 +0900

    JAVA 7일차 (23.12.28)_오류난거 수정

--- javastudy/src/main/java/com/kh/day7/VendingMachine/VendingMachine_Sol.java
+++ javastudy/src/main/java/com/kh/day7/VendingMachine/VendingMachine_Sol.java
@@ -1,4 +1,6 @@
 package com.kh.day7.VendingMachine;
+// 돈 투입시 에러발생 ! 문제점을 찾아라..
+// 찾음! minPrice() 사용시 길이를 prices[i]로 둬버림...
 
 import java.util.Scanner;
 
@@ -14,6 +16,7 @@ public class VendingMachine_Sol {
 
         boolean stop = false;
         while (!stop) {
+
             while (true) {
                 System.out.println("현재잔액 : " + balance);
                 System.out.print("투입액[0-종료] : ");
@@ -30,7 +33,7 @@ public class VendingMachine_Sol {
                 }
             } // while문 종료
 
-            if(stop) {
+            if (stop) {
                 break;
             }
 
@@ -45,20 +48,24 @@ public class VendingMachine_Sol {
                 }
             } while (true);
 
-            String selectedDrink = drinks[menu-1]; // 선택한 음료
-            int priceOfDrink = prices[menu-1]; // 선택한 음료 가격
+            String selectedDrink = drinks[menu - 1]; // 선택한 음료
+            int priceOfDrink = prices[menu - 1]; // 선택한 음료 가격
 
-            System.out.println(selectedDrink + "를 선택했습니다.");
-            balance -= priceOfDrink;
-            System.out.println("거스름돈 : " + balance);
-            balance = 0;
+            if (balance >= priceOfDrink) {
+                System.out.println(selectedDrink + "를 선택했습니다.");
+                balance -= priceOfDrink;
+                System.out.println("거스름돈 : " + balance);
+                balance = 0;
+            } else {
+                System.out.println("잔액이 부족합니다 " + selectedDrink + "은 " + priceOfDrink +"입니다.");
+            }
         }
     }
 
     // 최소가격을 찾는 메소드
     private int minPrice(int[] prices) {
         int min = prices[0];
-        for (int i = 0; i < prices[i]; i++) {
+        for (int i = 0; i < prices.length; i++) {
             if (min > prices[i]) {
                 min = prices[i];
             }
