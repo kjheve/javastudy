910020f60efc31a8210e323c892272cdf04bf9d4
kjheve <120702629+kjheve@users.noreply.github.com>
2024-01-03 09:37:08 +0900

    JAVA 9일차 (24.01.02)_수정

--- javastudy/src/main/java/com/kh/day9/exam4/Parent.java
+++ javastudy/src/main/java/com/kh/day9/exam4/Parent.java
@@ -1,10 +1,10 @@
 package com.kh.day9.exam4;
-
+// 상속이 안되는 것들 3가지
 public class Parent {
     String instanceField;
     public Parent() {super();} // 생성자 메소드
-    static String staticField; // 클래스 필드
     private String privateField; // private 필드
-    private void privateMethod(){} // 클래스 메소드
-    static void staticMethod(){} // private 메소드
+    private void privateMethod(){} // private 메소드
+    static void staticMethod(){} // 클래스 메소드 -> 상속 됨
+    static String staticField; // 클래스 필드 -> 상속 됨
 }
