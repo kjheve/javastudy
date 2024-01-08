d0694d216bf57dd0418019d4a2cd174620462614
kjheve <120702629+kjheve@users.noreply.github.com>
2024-01-08 16:11:34 +0900

    Delete javastudy/day11 directory

--- javastudy/day11/q6/TriangleDriver.java
+++
@@ -1,9 +0,0 @@
-package com.kh.day11.q6;
-
-public class TriangleDriver {
-    public static void main(String[] args) {
-        Triangle t1 = new Triangle(3, 5, 7);
-        Triangle t2 = new Triangle(3, 4, 5);
-        Triangle t3 = new Triangle(7, 10, 7);
-    }
-}
